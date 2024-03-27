788b52e3bbbd7048e988a23fe10c351e47c9060a
Pizza-Mozza <71978791+Pizza-Mozza@users.noreply.github.com>
2024-03-27 15:22:38 +0100

    commit manuel
    Flemme de merge

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+// Récupérer la liste des étudiants dans la table etudiant
+
+// 1. Connexion à la base de donnée db_intro
+/**
+ * @var PDO $pdo
+ */
+require './config/db-config.php';
+require_once 'header.php';
+
+
+// 2. Préparation de la requête
+$requete = $pdo->prepare(query: "SELECT * FROM film");
+
+// 3. Exécution de la requête
+$requete->execute();
+
+// 4. Récupération des enregistrements
+// Un enregistrement = un tableau associatif
+$films = $requete->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    <title>Filmosphère</title>
+</head>
+<body class=" bg-dark-subtle">
+<h1 class="text-center"><i class="bi bi-camera-reels-fill"></i> Bienvenue sur Filmosphère ! <i class="bi bi-camera-reels-fill"></i></h1>
+<h3 class="text-center">Dernières sorties </h3>
+<div class=" rounded-4 p-3 flex-fill">
+    <div class="container ">
+        <!-- Votre code -->
+        <div class="row text-center  ">
+            <p><i class="bi bi-camera-reels"></i> quoikoubekistant <i class="bi bi-camera-reels"></i></p>
+        </div>
+    </div>
+</div>
+<div class="d-flex mt-2">
+    <div class=" rounded-4 p-3 flex-fill">
+        <div class="container ">
+            <!-- Votre code -->
+            <div class="row text-center  ">
+                <?php foreach ($films as $film) : ?>
+                    <div class="card border-dark  mb-3 me-2 bg-light-subtle " style="max-width: 20rem;">
+                        <div class="card-body shadow-lg p-3 mb-5 bg-body rounded">
+                            <h4 class="card-title "><img class="w-100" src="<?= $film["image"] ?>" alt=""</h4>
+                            <p class="card-text"><?= $film["titre"] ?></p>
+                            <p> <?= $film["duree"] . " minutes" ?></p>
+                            <p class="card-text">
+                                <a class="btn btn-primary" role="button"
+                                   href="recup-param.php?id_film=<?= $film['id_film'] ?>
+                        ">Détails du film</a></p>
+
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+
+
+</body>
+</html>
---
+++ recup-param.php
@@ -0,0 +1,56 @@
+<?php
+// Récupère le paramètre d'URL 'prenom'
+// Tester la présence du paramètre
+// Récupère le paramètre d'URL 'prenom'
+// Tester la présence du paramètre
+if (isset($_GET['id_film'])) {
+    $id_film = $_GET['id_film'];
+
+// 1. Connexion à la base de donnée db_intro
+    require './config/db-config.php';
+
+// 2. Préparation de la requête
+    $requete = $pdo->prepare(query: "SELECT * FROM film WHERE id_film = :id");
+
+// 3. Lier le paramètre
+    $requete->bindParam(':id', $id_film);
+
+// 4. Exécution de la requête
+    $requete->execute();
+
+// 5. Récupération du film (vérifier si trouvé)
+    ?>
+
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+        <title>Document</title>
+    </head>
+    <body class="bg-secondary">
+    <div class="container text-center">
+    <div class="row p-5 d-md-block">
+    <div class=" ">
+    <?php
+    if ($film = $requete->fetch(PDO::FETCH_ASSOC)) { ?>
+        <?php echo "<img src='{$film['image']}' alt='' </img>"; ?>
+        </div>
+        <div class="text-md-center form-control ">
+        <?php
+        echo "<p>{$film['resume']}</p>"; // Assuming you have a "resume" attribute in your table
+    } else {
+        echo "Film introuvable";
+    }
+} else {
+    echo "Aucun ID de film fourni";
+}
+?>
+    </div>
+    </div>
+
+</body>
+    </html>
