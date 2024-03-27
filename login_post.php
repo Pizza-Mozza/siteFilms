b39438cf30a34638f1026675ec5153798378cc9f
Pizza-Mozza <michal.chilinski@yahoo.fr>
2024-03-27 17:36:29 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah

--- login_post.php
+++ login_post.php
@@ -1,3 +1,27 @@
+<?php
+session_start();  // démarrage d'une session
+
+// on vérifie que les données du formulaire sont présentes
+if (isset($_POST['login']) && isset($_POST['mdp'])) {
+    require 'fonctions.php';
+    $bdd = getBdd();
+    // cette requête permet de récupérer l'utilisateur depuis la BD
+    $requete = "SELECT * FROM utilis WHERE LoginUtil=? AND PassUtil=?";
+    $resultat = $bdd->prepare($requete);
+    $login = $_POST['login'];
+    $mdp = $_POST['mdp'];
+    $resultat->execute(array($login, $mdp));
+    if ($resultat->rowCount() == 1) {
+        // l'utilisateur existe dans la table
+        // on ajoute ses infos en tant que variables de session
+        $_SESSION['login'] = $login;
+        $_SESSION['mdp'] = $mdp;
+        // cette variable indique que l'authentification a réussi
+        $authOK = true;
+    }
+}
+?>
+
 <!doctype html>
 <html>
 <head>
@@ -8,11 +32,12 @@
 <h1>Résultat de l'authentification</h1>
 <?php
 if (isset($authOK)) {
-    echo "<p>Vous avez été reconnu(e) en tant que " . escape($email_utilisateur) . "</p>";
+    echo "<p>Vous avez été reconnu(e) en tant que " . escape($login) . "</p>";
     echo '<a href="index.php">Poursuivre vers la page d\'accueil</a>';
 }
 else { ?>
     <p>Vous n'avez pas été reconnu(e)</p>
-    <p><a href="public/login.php">Nouvel essai</p>
+    <p><a href="./public/login.php">Nouvel essai</p>
+<?php } ?>
 </body>
 </html>
--- public/login.php
+++ public/login.php
@@ -1,10 +1,14 @@
 <?php
-//session_start();  // démarrage d'une session
+session_start();  // démarrage d'une session
 require_once '../base.php';
 require_once BASE_PROJET .
     '/src/config/db-config.php';
 require_once BASE_PROJET .
     '/src/_partials/header.php';
+require_once BASE_PROJET .
+    '/fonctions.php';
+$_SESSION['email'] ='mc@feur.fr';
+$_SESSION['mdp'] ='X@mpp123';
 
 /**
  * @var PDO $pdo
@@ -28,10 +32,10 @@ $nom="";
 <body>
 <h1>Connexion utilisateur</h1>
 <form action="../login_post.php" method="post">
-    <label for="email_utilisateur">email :</label>
-    <input type="text" name="email_utilisateur" id="email_utilisateur" required />
-    <label for="mdp_utilisateur">Mot de passe :</label>
-    <input type="password" name="mdp_utilisateur" id="mdp_utilisateur" required />
+    <label for="email">email :</label>
+    <input type="text" name="email" id="email" required />
+    <label for="mdp">Mot de passe :</label>
+    <input type="password" name="mdp" id="mdp" required />
     <input type="submit" value="Connexion">
 </form>
 </body>
