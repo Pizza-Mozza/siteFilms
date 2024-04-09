59b4afafc99306997aa96e7a62489bc3368d0035
Pizza-Mozza <71978791+Pizza-Mozza@users.noreply.github.com>
2024-04-09 23:27:53 +0200

    ajouts depuis chez moi
    l'user peut ajouter des films

---
+++ login.php
@@ -0,0 +1,47 @@
+<?php
+session_start();  // démarrage d'une session
+
+if (!isset($_POST['email']) && !isset($_POST['mdp'])) {
+    // Détruire la session si l'utilisateur n'a pas soumis le formulaire de connexion
+    session_destroy();
+}
+
+require_once '../base.php';
+require_once BASE_PROJET .
+    '/src/config/db-config.php';
+require_once BASE_PROJET .
+    '/src/_partials/header.php';
+require_once BASE_PROJET .
+    '/public/fonctions.php';
+$_SESSION['email'] ='mc@feur.fr';
+$_SESSION['mdp'] ='X@mpp123';
+
+/**
+ * @var PDO $pdo
+ */
+
+$erreurs = [];
+$pseudo_utilisateur="";
+$email_utilisateur = "";
+$email_confirmation = "";
+$mdp_confirmation ="";
+$mdp_utilisateur ="";
+$prenom="";
+$nom="";
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Connexion</title>
+</head>
+<body>
+<h1>Connexion utilisateur</h1>
+<form action="/login_post.php" method="post">
+    <label for="email">email :</label>
+    <input type="text" name="email" id="email" required />
+    <label for="mdp">Mot de passe :</label>
+    <input type="password" name="mdp" id="mdp" required />
+    <input type="submit" value="Connexion">
+</form>
+</body>
